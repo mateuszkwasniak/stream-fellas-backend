40083a8d8afe1ff133d7689d36916bd90a0c0912
mateuszkwasniak <mz.kwasniak@gmail.com>
2023-06-26 11:08:18 +0200

    Nodemon setup + small config amendments

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const mongoose = require("mongoose");
+const connectMongoDB = require("./config/dbConnection");
 const PORT = process.env.PORT || 3500;
 
 connectMongoDB();
 app.use(cors(corsOptions));
 app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.status(200).json({ message: "Welcome test." });
+});
 
 app.all("*", (req, res) => {
   res.status(404).type("text/plain").send("404 Not Found");
